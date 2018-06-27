7cf41e3f43dd4f72fe8784e782378fc13ba38618
machester <shusheng1991@gmail.com>
2018-06-27 18:55:47 +0800

    2018-06-27

--- cpp_learning/test_code/class.cpp
+++ cpp_learning/test_code/class.cpp
@@ -1,4 +1,5 @@
-#include <iostream>
+#include <iostream>
+
 
 class Calc
 {
@@ -10,21 +11,37 @@ public:
 	~Calc();
 
 	int add(int param1, int param2);
+	int mulp(int param1, int param2);
+	double mulp(double param1, double param2);
 
 
 };
+
 Calc::Calc()
 {
 	std::cout << "--- check ---" << std::endl;
 }
+
 Calc::~Calc()
 {
-	std::cout << "end" << std::endl;
+	std::cout << "-------- end --------" << std::endl;
 }
+
 int Calc::add(int param1, int param2)
 {
 	return (param1 + param2);
 }
+
+int Calc::mulp(int param1, int param2)
+{
+	return (param1 * param2);
+}
+
+double Calc::mulp(double param1, double param2)
+{
+	return (param1 * param2);
+}
+
 
 int main(void)
 {
@@ -37,6 +54,14 @@ int main(void)
 		std::cout << "sum = ";
 		std::cout << sum;
 		std::cout << "\n";
+
+		std::cout   << "mult sum = "
+		            << calc.mulp(12, 3)
+		            << std::endl;
+
+		std::cout   << "mult sum = "
+		            << calc.mulp(12.2, 3.4)
+		            << std::endl;
 	}
 
 	return 0;
