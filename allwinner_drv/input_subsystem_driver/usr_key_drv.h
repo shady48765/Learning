174c6429e5f266935dad51d6acf976e23d2294b4
machester <shusheng1991@gmail.com>
2018-11-08 17:45:25 +0800

    driver dutty add

---
+++ allwinner_drv/input_subsystem_driver/usr_key_drv.h
@@ -0,0 +1,19 @@
+
+#ifndef __USR_KEY_DRV_H
+#define __USR_KEY_DRV_H
+
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/device.h>
+#include <linux/platform_device.h>
+
+
+
+
+
+
+
+
+
+
+#endif
