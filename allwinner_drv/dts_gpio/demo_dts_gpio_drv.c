ac806a7f65f3a701974c3121d9eda2ad336eee2e
machester <shusheng1991@gmail.com>
2018-11-10 17:33:05 +0800

    dts_gpio 2018-11-10

--- allwinner_drv/dts_gpio/demo_dts_gpio_drv.c
+++ allwinner_drv/dts_gpio/demo_dts_gpio_drv.c
@@ -1,14 +1,17 @@
 #include <linux/init.h>
 #include <linux/module.h>
-#include <linux/platform_device.h>
-#include <linux/property.h>
-#include <linux/device.h>
+#include <linux/kernel.h>
+#include <linux/platform_device.h>      /* For platform devices */
+#include <linux/interrupt.h>            /* For IRQ */
+#include <linux/gpio.h>                 /* For Legacy integer based GPIO */
+#include <linux/of_gpio.h>              /* For of_gpio* functions */
+#include <linux/of.h>                   /* For DT*/
+#include <linux/delay.h>
 #include <linux/fs.h>
-#include <linux/uaccess.h>
-#include <linux/slab.h>
-#include <linux/miscdevice.h>
+#include <linux/of_device.h>
 
-/*--------------------- macro defined start --------------------------*/
+/*--- global param declartion start ----------------*/
+/*--- macro defined start --------------------------*/
 #define DEV_DRV_NAME                "demo_dts_gpio_drv"
 #define DEVICE_NAME                 "demo_gpio_dev"
 
@@ -23,104 +26,124 @@
             }while(0)
 /*--------------------------- macro defined end ---------------------------*/
 
-static int gpio_dev_open (struct inode * node, struct file * files);
-static ssize_t gpio_dev_write (struct file *files, const char __user * buff, 
+static int laser_misc_open (struct inode * node, struct file * files);
+static ssize_t laser_misc_write (struct file *files, const char __user * buff, 
                                 size_t, loff_t * lofft);
-static long gpio_dev_iotcl (struct file *files, unsigned int cmd, unsigned long args);
-static int gpio_dev_release (struct inode * node, struct file * files);
+static long laser_misc_ioctl (struct file *files, unsigned int cmd, unsigned long args);
+static int laser_misc_release (struct inode * node, struct file * files);
 
 
-
-
-static const struct file_operations gpio_fops = {
-    .owner = THIS_MODULE,
-    .open = gpio_dev_open,
-    .write = gpio_dev_write,
-    .release = gpio_dev_release,
-    .unlocked_ioctl = gpio_dev_iotcl,
-};
+/*--- global param declartion end ----------------*/
 #if 0
-static struct miscdevice misc_dev = {
-    .minor = MISC_DYNAMIC_MINOR,
-    .name = DEVICE_NAME,
-    .fops = &gpio_fops,
-};
+struct pinctrl *pinctrl0;
+struct pinctrl_state *pins_default;
+struct pinctrl_state *laser;
 #endif
-struct _gpio_info {
-    struct miscdevice *gpio_misc_dev;
-};
 
-
-
-
-
-static int gpio_dev_open (struct inode * node, struct file * files)
+static int laser_misc_open (struct inode * node, struct file * files)
 {
     usr_msg("gpio port open.");
+    nonseekable_open(node, files);
     return 0;
 }
-static ssize_t gpio_dev_write (struct file *filp, const char __user * buff, 
+static ssize_t laser_misc_write (struct file *filp, const char __user * buff, 
                                 size_t size, loff_t * lofft)
 {
-    struct _gpio_info * gpio_info = container_of(filp->private_data, struct _gpio_info, gpio_misc_dev);
+    usr_msg("gpio port open.");
+    
+    mdelay(500);
+    //struct _gpio_info * gpio_info = container_of(filp->private_data, struct _gpio_info, gpio_misc_dev);
     
 
     
     return 0;
 }
-static long gpio_dev_iotcl (struct file *files, unsigned int cmd, unsigned long args)
+static long laser_misc_ioctl (struct file *files, unsigned int cmd, unsigned long args)
 {
 	return 0;
 }
-static int gpio_dev_release (struct inode * node, struct file * files)
+static int laser_misc_release (struct inode * node, struct file * files)
 {
-    // !!!!!!!!!!!!!! struct gpio_chip
+    
 	return 0;
 }
 
+#include <linux/pinctrl/consumer.h>
+#include <linux/miscdevice.h>
+#include <linux/pinctrl/pinctrl.h>
 
+struct laser_pin_info {
+struct pinctrl *pin;
+struct pinctrl_state *status;
+};
+struct laser_pin_info *pin_info;
+    
+static const struct file_operations laser_fops = {
+    .owner = THIS_MODULE,
+    .open = laser_misc_open,
+    .write = laser_misc_write,
+    .release = laser_misc_release,
+    .unlocked_ioctl = laser_misc_ioctl,
+};
+struct miscdevice laser_miscdev = {
+    .minor = MISC_DYNAMIC_MINOR,
+    .name = DEVICE_NAME,
+    .fops = &laser_fops,
+};
 
 static int gpio_pd_probe(struct platform_device * pd_dev)
 {
     int ret;
-    struct _gpio_info * gpio_info;
-    gpio_info = kmalloc(sizeof(struct _gpio_info), GFP_KERNEL);
-    if(!gpio_info) {
-        err_msg("kzmalloc error.");
-        ret = -ENOMEM;
-        goto err_kzmalloc;
-    }
-    
-    gpio_info->gpio_misc_dev->name = DEVICE_NAME;
-    gpio_info->gpio_misc_dev->minor = MISC_DYNAMIC_MINOR;
-    gpio_info->gpio_misc_dev->fops = &gpio_fops;
-    
-    ret = misc_register(gpio_info->gpio_misc_dev);
-    if (0 != ret) {
-        err_msg("misc register error.");
-        goto err_misc_register;
-    }
-    err_misc_register:
-        misc_deregister(gpio_info->gpio_misc_dev);
-        kfree(gpio_info);
-    err_kzmalloc:
+        
+    ret = misc_register(&laser_miscdev);
+    if(!ret) {
+        printk(KERN_ERR "error : misc_register failed.\n");
         return ret;
+    }
+
+    struct device *dev = &pd_dev->dev;
+        
+    /* Allocate a state holder named "laser_pin_info" etc */
+    pin_info = kmalloc(sizeof(struct laser_pin_info), GFP_KERNEL);
+    if(!pin_info) {
+        err_msg("error : malloc pin_info");
+        ret = -ENOMEM;
+        goto err_kmalloc;
+    }
+
+    pin_info->pin = devm_pinctrl_get(dev);
+    if (IS_ERR(pin_info->pin)) {
+        err_msg("error : cannot find device pin");
+        return PTR_ERR(pin_info->pin);
+    }
+
+    pin_info->status = pinctrl_lookup_state(pin_info->pin, PINCTRL_STATE_DEFAULT);
+    if (IS_ERR(pin_info->status)) {
+        printk(KERN_ERR "error : cat not find pin %s status\n", PINCTRL_STATE_DEFAULT);
+        return PTR_ERR(pin_info->status);
+    }
+
+    ret = pinctrl_select_state(pin_info->pin, pin_info->status);
+    if (ret < 0) {
+            printk(KERN_ERR "error : cat not set pin status.\n");
+            return ret;
+    }
+
+    
+
+err_kmalloc:
+    misc_deregister(&laser_miscdev);
+    return ret;
 }
 
 
 static int gpio_pd_remove(struct platform_device * pd_dev)
 {
-    struct _gpio_info * gpio_info = platform_get_drvdata(pd_dev);
-    misc_deregister(gpio_info->gpio_misc_dev);
-    kfree(gpio_info);
+    //struct _gpio_info * gpio_info = platform_get_drvdata(pd_dev);
+    misc_deregister(&laser_miscdev);
+    //kfree(gpio_info);
     return 0;
 }
-
-
-
-
-
-
 
 struct of_device_id laser_pin_match_table[] = {
     {.compatible = "nano, laser-pin",},
@@ -133,7 +156,7 @@ struct platform_driver gpio_pd_drv = {
     .driver = {
         .name = DEV_DRV_NAME,
         .owner = THIS_MODULE,
-        .of_match_table = laser_pin_match_table,
+        .of_match_table = of_match_ptr(laser_pin_match_table),
     },
     .probe = gpio_pd_probe,
     .remove = gpio_pd_remove,
@@ -141,6 +164,7 @@ struct platform_driver gpio_pd_drv = {
 
 module_platform_driver(gpio_pd_drv);
 
-MODULE_LICENSE("GPL");
-MODULE_AUTHOR("QUAN");
-MODULE_DESCRIPTION("gpio pin dts test");
+MODULE_AUTHOR("V");
+MODULE_DESCRIPTION("device tree gpio driver test");
+MODULE_LICENSE("GPL v2");
+MODULE_ALIAS("platform:" DEV_DRV_NAME);
