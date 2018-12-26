565f697bfcced600ee193efdeffe13eb2b477996
Jiang Quan <shusheng1991@gmail.com>
2018-12-26 18:30:44 +0800

    modify foo_proc.h

--- tinker/driver/file_system/proc/foo_proc.h
+++ tinker/driver/file_system/proc/foo_proc.h
@@ -17,7 +17,7 @@
 #include <linux/raid/pq.h>
 #include <linux/mutex.h>
 
-#define TIMER_TAG                   " <TIMER> "
+#define TIMER_TAG                   " <PROC> "
 #define USR_MSG_LEVEL               KERN_ERR
 #define USR_ERR_LEVEL               KERN_ERR
 #define usr_msg(fmt, args...)       printk(USR_MSG_LEVEL TIMER_TAG " (function : %s), [line : %d] "fmt"\n",__func__, __LINE__, ##args);
