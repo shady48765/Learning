467730cfcfcf465eaa12b66cb8e360578e851bfa
Jiang Quan <shusheng1991@gmail.com>
2019-01-22 18:19:38 +0800

    oled driver: need finished i2c data transfer

--- tinker/driver/i2c/oled/oled_operation.h
+++ tinker/driver/i2c/oled/oled_operation.h
@@ -31,31 +31,4 @@ char i2c_write_cmd(unsigned char i2c_cmd);
 
 
 
-
-
-// void oled_write_byte(unsigned dat,unsigned cmd);
-// void oled_show_char(unsigned char x,unsigned char y,
-//                             unsigned char chr,unsigned char char_size);
-
-// void oled_show_string(unsigned char x,unsigned char y,
-//                                 unsigned char *chr,unsigned char char_size);
-
-// void oled_set_pos(unsigned char x, unsigned char y);
-
-// void oled_init(void);
-// void oled_clear(void);
-// void oled_on(void);
-// void oled_off(void);
-// char write_i2c_data(unsigned char i2c_data);
-// int iic_write_reg(unsigned char cmd, unsigned char val);
-// char write_i2c_cmd(unsigned char i2c_cmd);
-// int i2c_master_reg8_send(const struct i2c_client *client, const char reg, 
-//                                         const char *buf, int count, int scl_rate);
-
-
-
-// extern struct _oled_i2c_info * oled_i2c_info;
-// extern struct _oled_dts_info * oled_dts_info;
-// extern struct _oled_device_info * oled_dev_info;
-
 #endif
