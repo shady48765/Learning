72eb391a22f7d8f9216be03da572bad2cd0a1020
machester <shusheng1991@gmail.com>
2019-05-29 19:15:12 +0800

    update driver xfm10213

--- xfm10213_driver/xfm10213.c
+++ xfm10213_driver/xfm10213.c
@@ -1,13 +1,7 @@
 
 #include "./xfm10213.h"
 
-static int upgrade_finished_mark;
 static int xfm10213_update_flag;
-
-static DECLARE_WAIT_QUEUE_HEAD(xfm10213_upgrade_work_head);
-
-static void xfm10213_init_worker(struct work_struct *work);
-static DECLARE_WORK(xfm10213_init_work, xfm10213_init_worker);
 
 
 static const struct of_device_id xfm10213_match_table[] = {
@@ -16,74 +10,35 @@ static const struct of_device_id xfm10213_match_table[] = {
 };
 MODULE_DEVICE_TABLE(of, xfm10213_match_table);
 
+#if 0
 static irqreturn_t xfm10213_irq_handler(int irq, void *data)
 {
 	struct xfm10213_info *info = data;
-	struct i2c_client *client = info->client;
+	// struct i2c_client *client = info->client;
 	usr_msg("moved in function: %s", __func__);
 	upgrade_finished_mark = 1;
 	disable_irq(info->irq_num);
-	schedule_work(xfm10213_irq_init);
+	// schedule_work(xfm10213_irq_init);
 	return IRQ_HANDLED;
 }
 
 static int xfm10213_irq_request(struct xfm10213_info *info)
 {
-	struct fsa9480_platform_data *pdata = usbsw->pdata;
-	struct i2c_client *client = usbsw->client;
-	int ret;
+	int ret, trigger_level;
 
 	usr_msg("moved in function: %s", __func__);
 	// using IRQF_ONESHOT to keep thread_handler full exit before accept next interrupt.
-	ret = request_threaded_irq(client->irq, NULL, xfm10213_irq_handler, IRQF_TRIGGER_RISING | IRQF_ONESHOT, "fsa9480 micro USB", usbsw);
+	trigger_level = info->irq_trigger_level ? IRQF_TRIGGER_RISING : IRQF_TRIGGER_FALLING;
+	ret = request_threaded_irq(info->irq_num, NULL, xfm10213_irq_handler, trigger_level | IRQF_ONESHOT, XFM10213_DEV_NAME, (void *)info);
 	if (ret) {
 		usr_msg("failed to request IRQ");
 		return ret;
 	}
 	return 0;
 }
+#endif
 
 
-static int xfm10213_get_dts_info(struct i2c_client * client, struct xfm10213_info * info)
-{
-	int ret;
-	enum of_gpio_flags flags;
-	char temp_str[20] = 0;
-	
-	usr_msg("moved in function: %s", __func__);
-	
-info->node = client->dev.of_node;
-	if(!info->node) {
-		usr_msg("cannot find device node");
-		return -ENODEV;
-	}
-	ret = of_get_named_gpio_flags(
-info->node, "xfm10213-irq-gpio", 0, &flags);
-	if(ret < 0) {
-		usr_msg("error: get xfm10213-irq-gpio");
-		return -EIO;
-	}
-	ret = of_property_read_string(info->node, "xfm10213-irq-trigger-level", &temp_str);
-	if(ret < 0) {
-		usr_msg("error: get xfm10213-irq-trigger-level");
-		return -EIO;
-	}
-	if((!temp_str) || (0 == strncmp("high", temp_str, 4))
-		info->irq_trigger_level = 1;
-	else
-		info->irq_trigger_level = 0;
-	
-	ret = of_property_read_u8(info->node, "reg", &info->addr);
-	if(ret < 0) {
-		usr_msg("error: get xfm10213-i2c-reg");
-		return -EIO;
-	}
-	info->irq_num = gpio_to_irq(info->irq_pin);
-
-	usr_msg("requested irq_pin = %d, irq_num = %d, setted irq-trigger-level = %s",
-					info->irq_pin, info->irq_num, info->irq_trigger_level ? "HIGH" : "LOW");
-	
-}
 
 static ssize_t xfm10213_update_set(struct device *dev, struct device_attribute *attr, const char *buf, size_t count)
 {
@@ -176,60 +131,218 @@ static const struct attribute_group xfm10213_attribute_group = {
 
 static void xfm10213_update_protocal(struct work_struct *work)
 {
-
-    wake_up_interruptible(&xfm10213_upgrade_work_head);
-    usr_msg("queue woke-up, xfm10213_update_flag = %d", xfm10213_update_flag);
+    usr_msg("xfm10213_update_protocal woke-up, xfm10213_update_flag = %d", xfm10213_update_flag);
     xfm10213_update_flag= 0;
-    schedule();
+
+	/** TODO */
 	
-    
 }
 
-void xfm10213_update_waitqueue_init(void)
+static DECLARE_WAIT_QUEUE_HEAD(xfm10213_upgrade_work_head);
+// static DECLARE_WAITQUEUE(name, tsk)
+void xfm10213_upgrade_work_init(struct xfm10213_info * info)
 {
-    init_waitqueue_head(&xfm10213_upgrade_work_head);
-    INIT_WORK(&xfm10213_init, xfm10213_update_protocal);
-    schedule_work(&xfm10213_init);
-    wait_event_interruptible(xfm10213_upgrade_work_head, xfm10213_update_flag != 0);
-    usr_msg("waitqueue, xfm10213_update_flag =  %d", xfm10213_update_flag);
+	info->xfm10213_upgrade_flag = 0;
+	xfm10213_update_flag = info->xfm10213_upgrade_flag;
+    INIT_WORK(info->xfm10213_upgrade_work, xfm10213_update_protocal);
+    schedule_work(info->xfm10213_upgrade_work);
+	wait_event_interruptible(xfm10213_upgrade_work_head, xfm10213_update_flag != 0);
 }
+void xfm10213_upgrade_work_remove(struct xfm10213_info * info)
+{
+	usr_msg("moved in function: %s", __func__);
+	// remove_wait_queue(&xfm10213_upgrade_work_head, wait_queue_t * wait)
+}
+
+
+unsigned int xfm10213_read_16bit(struct i2c_client *client, unsigned int sub_addr)
+{
+  	unsigned long rxdata;
+    unsigned char buff[4];
+    struct i2c_msg msgs[2];
+    struct i2c_adapter *adp;
 	
+    adp = to_i2c_adapter(client->dev.parent);
+
+    buff[0] = sub_addr >>8;
+    buff[1] = sub_addr & 0xff;
+    
+    msgs[0].addr  = client->addr;
+    msgs[0].flags = 0;
+    msgs[0].len   = 2;
+    msgs[0].buf   = buff;
+
+	memset(buff, 0, sizeof(buff));
+    msgs[1].addr  = client->addr;
+    msgs[1].flags = I2C_M_RD;
+    msgs[1].len   = 2;
+    msgs[1].buf   = buff;
+
+    if(unlikely(i2c_transfer(adp, msgs, 2) != 2)) {
+		usr_msg("error: rec 16bit message.");
+        return -1;
+    }
+
+	rxdata=(buff[0]<<8)|buff[1];
+    return rxdata;
+}
+int xfm10213_send_16bit(struct i2c_client *client, unsigned int sub_addr, unsigned int data)
+{
+	int ret;
+	unsigned char buff[4] = {0};
+	struct i2c_msg msg;
+	struct i2c_adapter *adp = client->adapter;
+
+	buff[0] = sub_addr >> 8;
+	buff[1] = sub_addr & 0xff;
+	buff[2] = data >> 8;
+	buff[3] = data & 0xff;
+
+	msg.addr = client->addr;
+	msg.flags = 0;
+	msg.len = 4;
+	msg.buf = buff;
+	ret = i2c_transfer(adp, &msg, 1);
+	if(ret < 0)
+		usr_msg("error: send 16bit message. ret = %d", ret);
+	return 0;
+}
+#if 0
+int xfm10213_send_str(struct i2c_client *client, unsigned char sub_addr, unsigned char data, int size)
+{
+	usr_msg("moved in function: %s", __func__);
+	return 0;
+	
+}
+
+int xfm10213_set_value(int gpio, gpio_dir dir, gpio_status value)
+{
+	int ret;
+	
+	if(in == dir) {
+		ret = gpio_direction_input(gpio);
+	} else {
+		ret = gpio_direction_output(gpio, value);
+	}
+	return ret;
+}
+#endif
+
+int xfm10213_chip_init(struct xfm10213_info * info)
+{
+	usr_msg("moved in function: %s", __func__);
+	xfm10213_send_16bit(info->client, UART_PRINT_CTRL_REG, UART_PRINT_HMI_MSG);
+	xfm10213_send_16bit(info->client, VOI_WORK_MODE_REG, VOI_WORK_TEST_MODE);
+
+	gpio_set_value(info->reset_pin, 0);
+	mdelay(50);
+	gpio_set_value(info->reset_pin, 1);
+	mdelay(100);
+	return 0;
+}
+static int xfm10213_get_dts_info(struct i2c_client * client, struct xfm10213_info * info)
+{
+	int ret;
+	const char * temp_str;
+	 struct device_node *node = NULL;
+	
+	usr_msg("moved in function: %s", __func__);
+	
+	// if(!client->dev.of_node) {
+	//	usr_msg("cannot find device node");
+	//	return -ENODEV;
+	//}
+	node = of_find_compatible_node(NULL, NULL, "xfm10213");
+	if (!node) {
+	usr_msg("cannot find node name rockchip,r100u-mcu-key");
+	return -ENODEV;
+	}
+	info->node = client->dev.of_node;
+	ret = of_get_named_gpio(info->node, "irq-gpio", 0);
+	if(ret < 0) {
+		usr_msg("error: get xfm10213-irq-gpio");
+		return -EIO;
+	}
+	ret = of_get_named_gpio(info->node, "reset", 0);
+	if(ret < 0) {
+		usr_msg("error: xfm10213-reset");
+		return -EIO;
+	}
+	ret = of_property_read_string(info->node, "irq-trigger-level", &temp_str);
+	if(ret < 0) {
+		usr_msg("error: get xfm10213-irq-trigger-level");
+		return -EIO;
+	}
+	if(0 == strncmp("low", temp_str, 3))
+		info->irq_trigger_level = 0;
+	else
+		info->irq_trigger_level = 1;
+	
+	// ret = of_property_read_u8(info->node, "reg", &info->addr);
+	// if(ret < 0) {
+	// 	usr_msg("error: get xfm10213-i2c-reg");
+	// 	return -EIO;
+	// }
+	info->irq_num = gpio_to_irq(info->irq_pin);
+	
+	ret = devm_gpio_request_one(&client->dev, info->irq_num, GPIOF_IN, "xfm10213-irq-gpio");
+	if(ret < 0) {
+		usr_msg("error: get xfm10213-i2c-reg");
+		return -EIO;
+	}
+	ret = devm_gpio_request_one(&client->dev, info->reset_pin, GPIOF_OUT_INIT_HIGH, "xfm10213-reset");
+	if(ret < 0) {
+		usr_msg("error: get xfm10213-i2c-reg");
+		goto out;
+	}
+	usr_msg("requested irq_pin = %d, irq_num = %d, setted irq-trigger-level = %s, reset-pin = %d",
+					info->irq_pin, info->irq_num, info->irq_trigger_level ? "HIGH" : "LOW",
+					info->reset_pin);
+	return 0;
+
+out:
+	devm_gpio_free(&client->dev, info->irq_pin);
+	return ret;
+}
+
 static int xfm10213_probe(struct i2c_client *client, const struct i2c_device_id *id)
 {
 	int ret;
 	// struct i2c_adapter *adapter = to_i2c_adapter(client->dev.parent);
-	struct i2c_adapter * adapter;
 	struct xfm10213_info * info = devm_kmalloc(&client->dev, sizeof(struct xfm10213_info), GFP_KERNEL);
-	if (!i2c_check_functionality(adapter, I2C_FUNC_SMBUS_BYTE_DATA)) {
+	usr_msg("moved in function: %s", __func__);
+	if (!i2c_check_functionality(client->adapter, I2C_FUNC_SMBUS_BYTE_DATA)) {
 		usr_msg("i2c check failed.");
 		return -EIO;
 	}
-
-	ret = xfm10213_get_dts_info(client, info)
+	usr_msg("i2c matched current slave addr is : 0x%x", client->addr);
+	info->client = client;
+	ret = xfm10213_get_dts_info(info->client, info);
 	if(ret < 0) {
 		usr_msg("error: get dts info");
 		return -EIO;
 	}
-	info->client = client;
-    mutex_init(&info->lock);
-
-	xfm10213_update_waitqueue_init();
+	xfm10213_chip_init(info);
 	
-	sysfs_create_group(&client->dev.kobj, &xfm10213_attribute_group);
-	return 0;
+	info->init_flag = 0;
+	
+    mutex_init(&info->lock);
+	
+	i2c_set_clientdata(client, info);
+//	i2c_set_adapdata(struct i2c_adapter * dev, void * data);
+ 	
+	xfm10213_upgrade_work_init(info);
+	ret = sysfs_create_group(&client->dev.kobj, &xfm10213_attribute_group);
 
 	return ret;
+
 }
 
 static int xfm10213_remove(struct i2c_client *client)
 {
-	struct fsa9480_usbsw *usbsw = i2c_get_clientdata(client);
-	if (client->irq)
-		free_irq(client->irq, usbsw);
-
-	sysfs_remove_group(&client->dev.kobj, &fsa9480_group);
-	device_init_wakeup(&client->dev, 0);
-	kfree(usbsw);
+	struct xfm10213_info *info = i2c_get_clientdata(client);
+	free_irq(info->irq_num, NULL);
+	sysfs_remove_group(&client->dev.kobj, &xfm10213_attribute_group);
 	return 0;
 }
 
@@ -243,20 +356,21 @@ static struct i2c_driver xfm10213_driver = {
 	.driver = {
 		.name = XFM10213_DEV_NAME,
 		.owner = THIS_MODULE,
-		.of_match_table = of_match_ptr(xfm10213_match_table);
+		.of_match_table = of_match_ptr(xfm10213_match_table),
 	},
 	.probe = xfm10213_probe,
 	.remove = xfm10213_remove,
-	.id = xfm10213_dev_id,
+	.id_table = xfm10213_dev_id,
 };
 
 static int __init xfm10213_init(void)
 {
 	usr_msg("moved in function: %s", __func__);
 	i2c_add_driver(&xfm10213_driver);
+	return 0;
 }
 
-static void _exit xfm10213_exit(void)
+static void __exit xfm10213_exit(void)
 {
 	usr_msg("moved in function: %s", __func__);
 	i2c_del_driver(&xfm10213_driver);
--- xfm10213_driver/xfm10213.h
+++ xfm10213_driver/xfm10213.h
@@ -14,7 +14,7 @@
 #include <linux/module.h>
 #include <linux/err.h>
 #include <linux/i2c.h>
-#include <linux/platform_data/fsa9480.h>
+#include <linux/i2c-dev.h>
 #include <linux/irq.h>
 #include <linux/interrupt.h>
 #include <linux/workqueue.h>
@@ -22,11 +22,16 @@
 #include <linux/slab.h>
 #include <linux/pm_runtime.h>
 #include <linux/of.h>
-
+#include <linux/mutex.h>
+#include <linux/of_gpio.h>
+#include <linux/wait.h>
+#include <linux/workqueue.h>
+#include <linux/string.h>
+#include <linux/delay.h>
 
 #define XFM10213_DEV_NAME				"xfm10213"
 
-#define TAG								"<XFM10213>"
+#define TAG								"<XFM10213> "
 #define SET_LEVEL						KERN_INFO
 #define usr_msg(fmt, args...)			printk(SET_LEVEL TAG fmt"\n", ##args)
 
@@ -34,38 +39,38 @@
 // #define 	XFM10213_ADDR				(0x7C)	// 8bit wirte address
 // #define 	XFM10213_ADDR				(0x7D)	// 8bit read address
 
-/** start: I2S configuration register ------------------------------------------*/
+/** start: I2S configuration register --------------------------------------------------*/
 #define 	I2S_CONFIG_REG 	 			(0x011B)
 #define 	I2S_MASTER_MODE				(0x0000)
 #define 	I2S_SLAVE_MODE				(0x0011)
-/** end: I2S configuration register --------------------------------------------*/
-/** start: UART speed configuration register ------------------------------------------*/
+/** end: I2S configuration register -----------------------------------------------------*/
+/** start: UART speed configuration register --------------------------------------------*/
 #define 	UART_SPEED_CONFIG_REG 	 	(0x011D)
 #define 	UART_SPEED_9600				(0x0060)
 #define 	UART_SPEED_115200			(0x00C0)
 #define 	UART_SPEED_256000			(0x0480)
-/** end: UART speed configuration register --------------------------------------------*/
+/** end: UART speed configuration register -----------------------------------------------*/
 /** start: UART priority configuration register ------------------------------------------*/
 #define 	UART_PRINT_CTRL_REG 	 	(0x011C)
 #define 	UART_PRINT_CORE_MSG 	 	(0x0000)
 #define 	UART_PRINT_ERR_MSG			(0x0001)
 #define 	UART_PRINT_HMI_MSG			(0x0002)
 #define 	UART_PRINT_ALL_MSG			(0x0003)
-/** end: UART priority configuration register --------------------------------------------*/
-/** start: MIC distance configuration register ------------------------------------------*/
+/** end: UART priority configuration register ---------------------------------------------*/
+/** start: MIC distance configuration register --------------------------------------------*/
 #define 	MIC_DISTANCE_REG 	 		(0x0116)
 #define 	MIC_DISTANCE_MIN			(0x0014)
 #define 	MIC_DISTANCE_MAX			(0x0078)
-/** end: MIC distance configuration register --------------------------------------------*/
+/** end: MIC distance configuration register -----------------------------------------------*/
 /** start: MIC output gain configuration register ------------------------------------------*/
 #define 	MIC_OUT_GAIN_REG 	 		(0x011A)
 /** end: MIC output gain configuration register --------------------------------------------*/
-/** start: MIC wake configuration register ------------------------------------------*/
+/** start: MIC wake configuration register -------------------------------------------------*/
 #define 	MIC_WAKEUP_REG 	 			(0x0113)
-/** end: MIC wake configuration register --------------------------------------------*/
-/** start: voice channel words register ------------------------------------------*/
+/** end: MIC wake configuration register ---------------------------------------------------*/
+/** start: voice channel words register ----------------------------------------------------*/
 #define 	VOI_CHANNEL_REG 	 		(0x0111)
-/** end: voice channel words register --------------------------------------------*/
+/** end: voice channel words register ------------------------------------------------------*/
 
 /** start: xfm10213 work mode configuration register ------------------------------------------*/
 #define 	VOI_WORK_MODE_REG 	 		(0x0110)
@@ -87,33 +92,50 @@
 #define 	VOI_SYS_VERSION_CHECK					(0x010A)
 
 typedef enum {
-	master = 0;
-	slave = !master;
+	low = 0,
+	high = !low
+} gpio_status;
+	
+typedef enum {
+	in = 0,
+	out = !in
+}gpio_dir;
+	
+typedef enum {
+	master = 0,
+	slave = !master
 } xfm10213_work_mode;
 
 struct xfm10213_info {
 	int irq_pin;
+	int reset_pin;
 	int irq_num;
 	int irq_trigger_level;
-	int update_flag;
+	int xfm10213_upgrade_flag;
 	int init_flag;
 	
 	xfm10213_work_mode mode;
 	
-	unsigned char addr;
-	mutex lock;
+	unsigned char 	addr;
+	struct mutex 	lock;
 	
 	struct i2c_client * client;
 	struct device_node * node;
-	proc_dir_entry * xfm10213_proc_entry;
+	struct proc_dir_entry * xfm10213_proc_entry;
+	struct work_struct * xfm10213_upgrade_work;
 };
 
 
-unsigned char * xfm10213_read_byte(struct i2c_client *client, unsigned char sub_addr, unsigned char data);
-int xfm10213_send_byte(struct i2c_client *client, unsigned char sub_addr, unsigned char data);
+unsigned int xfm10213_read_16bit(struct i2c_client *client, unsigned int sub_addr);
+int xfm10213_send_16bit(struct i2c_client *client, unsigned int sub_addr, unsigned int data);
+// int xfm10213_send_str(struct i2c_client *client, unsigned int sub_addr, unsigned int data, unsigned int size);
 xfm10213_work_mode xfm10213_get_current_work_mode(struct xfm10213_info);
-int xfm10213_set_to_mode(struct xfm10213_info * info, xfm10213_info mode);
-int xfm10213_create_proc_file(struct xfm10213_info * info);
-int xfm10213_remove_proc_file(struct xfm10213_info * info);
+int xfm10213_set_to_mode(struct xfm10213_info * info, xfm10213_work_mode mode);
+void xfm10213_upgrade_work_init(struct xfm10213_info * info);
+void xfm10213_upgrade_work_remove(struct xfm10213_info * info);
+// int xfm10213_proc_create(struct xfm10213_info * info);
+// int xfm10213_proc_remove(struct xfm10213_info * info);
+int xfm10213_chip_init(struct xfm10213_info * info);
+int xfm10213_set_value(int gpio, gpio_dir dir, gpio_status value);
 
 #endif
