04b193398f19cd5df644a619e3e3044062a4514a
machester <shusheng1991@gmail.com>
2019-08-26 17:06:58 +0800

    device_create_file error need show and store

--- andriod_kernel/key_trigger_auto_rerboot_test/keyset_reboot_test.c
+++ andriod_kernel/key_trigger_auto_rerboot_test/keyset_reboot_test.c
@@ -35,7 +35,12 @@ reboot_test: reboot-test {
 #include <linux/delay.h>
 #include <linux/sysfs.h>
 #include <linux/device.h>
+
+
 #include <linux/kernel.h>
+#include <linux/ctype.h>
+#include <linux/leds.h>
+
 
 static int debug_status = 1;
 
@@ -49,7 +54,12 @@ static int debug_status = 1;
                                                 printk(SET_LEVEL TAG fmt"\n", ##args);      \
                                         } while(0)
 
-#define KEY_DEV_NAME						"r101u-test-key"			 
+#define KEY_DEV_NAME						"r101u-test-key"			
+
+#define USING_SYS_FS					0
+#if USING_SYS_FS
+static int reboot_status_flag;
+#endif
 
 typedef enum {
 	low = 0,
@@ -61,8 +71,16 @@ typedef enum {
 	out = !in
 }gpio_dir;
 
-static int reboot_key_set_value(int gpio, gpio_dir dir, gpio_status value);
-static irqreturn_t mcu_trigger_handler(int irq, void *arg);
+typedef enum {
+	standby = 0,
+	running = 1,
+	pass	= 2,
+	failed  = 3
+}test_status;
+
+
+
+static irqreturn_t reboot_trigger_handler(int irq, void *arg);
 
 static const struct of_device_id reboot_key_match_table[] = {
 	{ .compatible = "rockchip, reboot-test", },
@@ -80,57 +98,35 @@ struct reboot_key_info {
 	struct workqueue_struct 	* wq;
 	struct work_struct			work;
 	struct mutex				tim_lock;
+	
 };
 
 /**----------------------------------------------------------------------------------*/
 
-static int reboot_key_set_value(int gpio, gpio_dir dir, gpio_status value)
-{
-	int ret;
-	
-	if(in == dir) {
-		ret = gpio_direction_input(gpio);
-	} else {
-		ret = gpio_direction_output(gpio, value);
-	}
-	return ret;
-}
-
-static int reboot_key_chip_init(struct reboot_key_info * info)
-{
-	int ret;
-	ret = reboot_key_set_value(info->mcu_p42_gpio, out, high);
-	ret = ret || reboot_key_set_value(info->mcu_p53_gpio, in, low);	
-	if(ret < 0) {
-		usr_msg("gpio: [%d], gpio: [%d] set direction failed.", info->mcu_p42_gpio, info->mcu_p53_gpio);
-		return ret; 
-	}
-	debug_msg("gpio set pass, current mcu_p53_gpio = %d", gpio_get_value(info->mcu_p53_gpio));
-	return 0;
-}
-
 static void data_handler(struct work_struct * work)
 {
 	int val;
-	struct reboot_key_info * btn = container_of(work, struct reboot_key_info, work);
-	if(IS_ERR(btn)) {
+	struct reboot_key_info * info = container_of(work, struct reboot_key_info, work);
+	if(IS_ERR(info)) {
 		usr_msg("error: get struct usr_keys_button in function: %s", __func__);
 		return ;
 	}
 	usr_msg("moved in function: %s", __func__);
 
-    mutex_lock(&btn->lock);
-    mdelay(1000);
-    val = gpio_get_value(btn->mcu_p53_gpio);
+    mutex_lock(&info->lock);
+    mdelay(info->detect_interval);
+    val = gpio_get_value(info->reboot_trigger_io);
     debug_msg("pressed, report KEY_CODE = %d, KEY_VALUE = %d", KEY_VAL, val);
-    mdelay(1000);
-    input_report_key(btn->inputdev, KEY_VAL, val);
-    input_sync(btn->inputdev);
+    input_report_key(info->inputdev, KEY_VAL, val);
+    input_sync(info->inputdev);
+    debug_msg("release, report KEY_CODE = %d, KEY_VALUE = %d", KEY_VAL, !val);
+    input_report_key(info->inputdev, KEY_VAL, !val);
+    input_sync(info->inputdev);
 
-    mutex_unlock(&btn->lock);
+    mutex_unlock(&info->lock);
 }
 
-static irqreturn_t mcu_trigger_handler(int irq, void *arg)
+static irqreturn_t reboot_trigger_handler(int irq, void *arg)
 {
 	struct reboot_key_info * info = (struct reboot_key_info *)arg;
 	if(!info) {
@@ -138,7 +134,7 @@ static irqreturn_t mcu_trigger_handler(int irq, void *arg)
 		return IRQ_HANDLED;
 	}
 	debug_msg("triggered");
-	disable_irq_nosync(irq);
+	// disable_irq_nosync(irq);
 	schedule_work(&info->work);
     return IRQ_HANDLED; 
 }
@@ -152,88 +148,169 @@ static int usr_get_dts_info(struct reboot_key_info * info, struct device *dev)
 		usr_msg("cannot find node name rockchip, reboot-test");
 		return -ENODEV;
 	}
-	info->key_gpio_num = of_get_named_gpio(node, "reboot-key-gpios", 0);
-	if(!gpio_is_valid(info->key_gpio_num)) {
-		usr_msg("error: get reboot-key-gpios gpio info. ERROR code %d", info->key_gpio_num);
+	info->reboot_trigger_io = of_get_named_gpio(node, "reboot-key", 0);
+	if(!gpio_is_valid(info->reboot_trigger_io)) {
+		usr_msg("error: get reboot-key gpio info. ERROR code %d", info->reboot_trigger_io);
 		return -EIO;
 	}
-    info->led_gpio_num = of_get_named_gpio(node, "led-gpio", 0);
-    if(!gpio_is_valid(info->led_gpio_num)) {
-        usr_msg("error: get info->led_gpio_num info.  ERROR code %d", info->led_gpio_num);
-        return -EIO;
+	ret = of_property_read_u32(node, "detect-interval", &info->detect_interval);
+    if(ret < 0) {
+        usr_msg("error: read detect-interval.  ERROR code %d", info->detect_interval);
+        return -EINVAL;
     }
-	ret = devm_gpio_request_one(dev, info->key_gpio_num, GPIOF_IN, "reboot-key-gpios");
+	ret = devm_gpio_request_one(dev, info->reboot_trigger_io, GPIOF_IN, "reboot-test-key-gpios");
     if(ret < 0) {
 		usr_msg("error: reboot-key-gpios request.");
 		return -EIO;
 	}
-	ret = devm_gpio_request_one(dev, info->key_gpio_num, GPIOF_OUT_INIT_LOW, "led_gpio_num");
-    if(ret < 0) {
-		usr_msg("error: key_gpio_num request.");
-		return -EIO;
-	}
-
-	debug_msg("requested key_gpio_num number: %d, led_gpio_num number: %d", info->key_gpio_num, info->led_gpio_num);
-	ret = reboot_key_chip_init(info);
-	if(ret < 0)
-		return -EIO;
+	debug_msg("requested reboot_trigger_io number: %d, info->detect_interval = %ul\n", info->reboot_trigger_io, info->detect_interval);
 	
 	return 0;
 
 }
 
-
- static ssize_t led_reboot_start(struct device *dev, struct device_attribute*attr,const char *buf,size_t count)
+#if USING_SYS_FS
+ static ssize_t reboot_running(struct device *dev, struct device_attribute *attr, const char *buf, size_t size)
  {
- 	debug_msg("moved in function: %s", __func__);
-	
+ 	unsigned long state, timer_on_ticks, timer_off_ticks;
+	struct led_classdev *led_cdev = dev_get_drvdata(dev);
+	ssize_t ret = -EINVAL;
+	timer_on_ticks = 200;
+	timer_off_ticks = 2000;
+	debug_msg("---> moved in function: %s", __func__);
+	ret = kstrtoul(buf, 10, &state);
+	if (ret)
+		return ret;
+	debug_msg("---> in function: %s, state = %d", __func__);
+	if(state) {
+		led_blink_set(led_cdev, &timer_on_ticks, &led_cdev->blink_delay_off);
+		led_cdev->blink_delay_on = timer_on_ticks;
+		led_blink_set(led_cdev, &led_cdev->blink_delay_on, &timer_off_ticks);
+		led_cdev->blink_delay_off = timer_off_ticks;
+	}
+	reboot_status_flag = 1;
+	return size;
  }
 
- static ssize_t led_reboot_running(struct device *dev, struct device_attribute*attr,const char *buf,size_t count)
- {
- 
- }
-
- static ssize_t led_reboot_pass(struct device *dev, struct device_attribute*attr,const char *buf,size_t count)
+ static ssize_t reboot_pass(struct device *dev, struct device_attribute *attr, const char *buf, size_t size)
 {
+	 struct led_classdev *led_cdev = dev_get_drvdata(dev);
+	 unsigned long state, timer_on_ticks, timer_off_ticks;
+	 ssize_t ret = -EINVAL;
+	 timer_on_ticks = 2000;
+	 timer_off_ticks = 2000;
+	 debug_msg("---> moved in function: %s", __func__);
+	 ret = kstrtoul(buf, 10, &state);
+	 if (ret)
+		 return ret;
+	 debug_msg("---> in function: %s, state = %d", __func__);
+	 if(state) {
+		 led_blink_set(led_cdev, &timer_on_ticks, &led_cdev->blink_delay_off);
+		 led_cdev->blink_delay_on = timer_on_ticks;
+		 led_blink_set(led_cdev, &led_cdev->blink_delay_on, &timer_off_ticks);
+		 led_cdev->blink_delay_off = timer_off_ticks;
+	 }
+	 reboot_status_flag = 2;
+	 return size;
 
 }
  
-static ssize_t led_reboot_failed(struct device *dev, struct device_attribute*attr,const char *buf,size_t count)
+static ssize_t reboot_failed(struct device *dev, struct device_attribute *attr, const char *buf, size_t size)
 {
+	 
+	struct led_classdev *led_cdev = dev_get_drvdata(dev);
+	unsigned long state, timer_on_ticks, timer_off_ticks;
+	ssize_t ret = -EINVAL;
+	timer_on_ticks = 200;
+	timer_off_ticks = 200;
+	debug_msg("---> moved in function: %s", __func__);
+	ret = kstrtoul(buf, 10, &state);
+	 if (ret)
+		 return ret;
+	 debug_msg("---> in function: %s, state = %d", __func__);
+	 if(state) {
+		 led_blink_set(led_cdev, &timer_on_ticks, &led_cdev->blink_delay_off);
+		 led_cdev->blink_delay_on = timer_on_ticks;
+		 led_blink_set(led_cdev, &led_cdev->blink_delay_on, &timer_off_ticks);
+		 led_cdev->blink_delay_off = timer_off_ticks;
+	 }
+	 reboot_status_flag = 3;
+	 return size;
 
 }
-
-static ssize_t reboot_test_status(struct device *dev, struct device_attribute*attr, char *buf)
+static ssize_t reboot_status(struct device *dev, struct device_attribute*attr, char *buf)
 {
-
+	if(0 == reboot_status_flag)
+		return sprintf(buf, "%s\n", "standby");
+	else if(1 == reboot_status_flag)
+		return sprintf(buf, "%s\n", "running");
+	else if(2 == reboot_status_flag)
+		return sprintf(buf, "%s\n", "pass");
+	else if(3 == reboot_status_flag)
+		return sprintf(buf, "%s\n", "failed");
+	return 0;
+}
+#if 0
+static ssize_t reboot_status_store(struct device *dev, struct device_attribute *attr, const char *buf, size_t size)
+{
+	 return size;
 }
 
+static ssize_t reboot_running_show(struct device *dev, struct device_attribute*attr, char *buf)
+{
+	return sprintf(buf, "%s\n", "not use");
+}static ssize_t reboot_pass_show(struct device *dev, struct device_attribute*attr, char *buf)
+{
+	return sprintf(buf, "%s\n", "not use");
+}
+static ssize_t reboot_failed_failed(struct device *dev, struct device_attribute*attr, char *buf)
+{
+	return sprintf(buf, "%s\n", "not use");
+}
+#endif
 
-static DEVICE_ATTR(led_start_reboot, 			S_IWUSR | S_IRUGO, NULL, led_reboot_start);
-static DEVICE_ATTR(led_runing_reboot,      		S_IWUSR | S_IRUGO, NULL, led_reboot_running);
-static DEVICE_ATTR(led_test_pass_reboot,        S_IWUSR | S_IRUGO, NULL, led_reboot_pass);
-static DEVICE_ATTR(led_test_failed_reboot,   	S_IWUSR | S_IRUGO, NULL, led_reboot_failed);
-static DEVICE_ATTR(reboot_status,  	 			S_IWUSR | S_IRUGO, reboot_test_status, NULL);
-
-
-static struct attribute *reboot_led_attributes[] = {
-    &dev_attr_led_start_reboot.attr,
-    &dev_attr_led_runing_reboot.attr,
-    &dev_attr_led_test_pass_reboot.attr,
-    &dev_attr_led_test_failed_reboot.attr,
-    &dev_attr_reboot_status.attr,
-    NULL
-};
-
-
+static DEVICE_ATTR(reboot_test_running,     0666, NULL, reboot_running);
+static DEVICE_ATTR(reboot_test_pass,        0666, NULL, reboot_pass);
+static DEVICE_ATTR(reboot_test_failed,   	0666, NULL, reboot_failed);
+static DEVICE_ATTR(reboot_test_status,  	0666, reboot_status, NULL);
 
 static int reboot_key_sysfs_create(struct platform_device * pdev)
 {
-	sysfs_create_group(&pdev->dev.kobj, &reboot_test_status_attr_group);
-	device_create_file(struct device * dev, const struct device_attribute * attr);
+	int rc;
+	
+	rc = device_create_file(&pdev->dev, &dev_attr_reboot_test_running);
+	if (rc) {
+		debug_msg("---> in function: %s, create fs led_runing_reboot failed", __func__);
+		return -1;
+	}
+	rc = device_create_file(&pdev->dev, &dev_attr_reboot_test_pass);
+	if (rc) {
+		debug_msg("---> in function: %s, create fs dev_attr_led_test_pass_reboot failed", __func__);
+		return -1;
+	}
+	rc = device_create_file(&pdev->dev, &dev_attr_reboot_test_failed);
+	if (rc) {
+		debug_msg("---> in function: %s, create fs dev_attr_led_test_failed_reboot failed", __func__);
+		return -1;
+	}
+	rc = device_create_file(&pdev->dev, &dev_attr_reboot_test_status);
+	if (rc) {
+		debug_msg("---> in function: %s, create fs dev_attr_reboot_status failed", __func__);
+		return -1;
+	}
+	
+	return 0;
+	
 }
 
+static void reboot_key_sysfs_remove(struct platform_device * pdev)
+{
+	device_remove_file(&pdev->dev, &dev_attr_reboot_test_running);
+	device_remove_file(&pdev->dev, &dev_attr_reboot_test_pass);
+	device_remove_file(&pdev->dev, &dev_attr_reboot_test_failed);
+	device_remove_file(&pdev->dev, &dev_attr_reboot_test_status);
+}
+#endif
 
 static int reboot_key_probe(struct platform_device *pdev)
 {
@@ -291,8 +368,8 @@ static int reboot_key_probe(struct platform_device *pdev)
 	flush_workqueue(info->wq);
 	INIT_WORK(&info->work, data_handler);
 
-	info->irq = gpio_to_irq(info->mcu_p53_gpio);
-	ret = request_irq(info->irq, mcu_trigger_handler, IRQF_TRIGGER_HIGH, "reboot_key_irq", (void *)info);
+	info->irq = gpio_to_irq(info->reboot_trigger_io);
+	ret = request_irq(info->irq, reboot_trigger_handler, IRQF_TRIGGER_FALLING, "reboot_key_irq", (void *)info);
 	if(ret < 0) {
 		usr_msg("error: request irq");
 		goto error3;
@@ -300,6 +377,10 @@ static int reboot_key_probe(struct platform_device *pdev)
 	debug_msg("requested irq number = %d", info->irq);
 	platform_set_drvdata(pdev, (void *) info);
 	dev_set_drvdata(&pdev->dev, (void *) info);
+	
+#if USING_SYS_FS
+	reboot_key_sysfs_create(pdev);
+#endif
 	return 0;
 
 
@@ -322,9 +403,13 @@ static int reboot_key_remove(struct platform_device *pdev)
 	cancel_work_sync(&btn->work);
 	flush_workqueue(btn->wq);
 	destroy_workqueue(btn->wq);
-
 	input_unregister_device(btn->inputdev);
+	devm_gpio_free(&pdev->dev, btn->reboot_trigger_io);
 	
+#if USING_SYS_FS
+	reboot_key_sysfs_remove(pdev);
+#endif
+
 	return 0;
 }
 
